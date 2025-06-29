021b769d3393402887ae7b217e4810832802d884
Tuccster <coleslater50@gmail.com>
2025-06-29 11:14:35 -0700

    process image through vite

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <!-- src/App.svelte -->
 <script lang="ts">
-
+  import dogImage from './assets/dog.jpg';
 </script>
 
 <div class="container mx-auto px-4"> 
   <section class="h-screen">
     <h1 class="font-extrabold justify-self-center text-7xl py-5">⚠ STINKY DOG ⚠</h1>
-    <img src="src/assets/dog.jpg" alt="stinky dog" class="size-150 justify-self-center border-amber-300 border-8"/>
+    <img src={dogImage} alt="stinky dog" class="size-150 justify-self-center border-amber-300 border-8"/>
     <h1 class="font-extrabold justify-self-center text-7xl py-5">⚠ STINKY DOG ⚠</h1>
   </section>
 </div>
